50b82d08faea488da86e9076cda53a6fae87e007
mikeuseni <77750051+Mike1908@users.noreply.github.com>
2021-09-12 14:43:14 -0400

    Updates

---
+++ precache-manifest.650f97080ee915ab76abadbaf8152a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab87ddf02eb8023abace394b9db3627",
+    "url": "/chatbox-app/index.html"
+  },
+  {
+    "revision": "a45a7022e9e1efa7cac6",
+    "url": "/chatbox-app/static/css/main.bec753e9.chunk.css"
+  },
+  {
+    "revision": "a9a179b4fd43e89b3a55",
+    "url": "/chatbox-app/static/js/2.c8a519c9.chunk.js"
+  },
+  {
+    "revision": "d662384d41d8fabd8bf6e8d18a478f90",
+    "url": "/chatbox-app/static/js/2.c8a519c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45a7022e9e1efa7cac6",
+    "url": "/chatbox-app/static/js/main.37dc602c.chunk.js"
+  },
+  {
+    "revision": "e62d52307f055dcdb613",
+    "url": "/chatbox-app/static/js/runtime-main.ba198257.js"
+  }
+]);
